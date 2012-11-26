907087b5999960988b148da3642247af7ddf2d0d
unknown <g35079@L401P15.pdg.esi.heb.site>
2012-11-26 09:57:40 +0100

    t

---
+++ MarkESI-client-web/web/WEB-INF/js/file.js
@@ -0,0 +1,53 @@
+function addClass(curr_annot, curr_id_sel, selection, class_to_add) {
+    var div_content = $('#code #content'); //on met à jour div_content
+    var text = div_content.html();
+
+    //on trouve le vrai début de la sélection dans le code html
+    var start = findIndex(selection.start, text);
+    var end = findIndex(selection.end, text);
+
+    var html_before = text.substring(0, start);
+    var html_middle = text.substring(start, end);
+    var html_after = text.substring(end, text.length);
+    
+    var new_html = html_before + '<span id="annot_' + curr_annot + '_sel_' + curr_id_sel + '" class="' + class_to_add + '">' + html_middle + '</span>' + html_after;
+    div_content.html(new_html);
+}
+    
+function removeClass(id_selection, curr_annot) {
+    var to_replace = $('#annot_' + curr_annot + '_sel_' + id_selection);
+    to_replace.replaceWith(to_replace.html());
+}
+
+function findIndex(index, text) {
+    var i = 0;
+    var nb_char_balises = 0;
+    var trouve = false;
+
+    while (!trouve && i < text.length) {
+        if ((i - nb_char_balises) == index) {
+            trouve = true;
+        } else if (text.charAt(i) == '&') {
+            //c'est un seul caractère transformé en non html
+            while (text.charAt(i) != ';') {
+                //on le passe complètement en le comptant comme non caractère
+                i++;
+                nb_char_balises++;
+            }
+            i++;
+        } else if (text.charAt(i) == '<') {
+            //c'est un <span> en html
+            while (text.charAt(i) != '>') {
+                //on le passe en le comptant comme balises
+                i++;
+                nb_char_balises++;
+            }
+            i++;
+            nb_char_balises++;
+        } else {
+            i++;
+        }
+    }
+
+    return i;
+}
