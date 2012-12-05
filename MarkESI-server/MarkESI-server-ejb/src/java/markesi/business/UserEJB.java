2d77f21c931eca2dcabc03d895bfe327ad67e4ba
g30917 <g30917@L401P16.pdg.esi.heb.site>
2012-12-05 14:23:06 +0100

    ajout user entity, ejb etc

---
+++ MarkESI-server/MarkESI-server-ejb/src/java/markesi/business/UserEJB.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package markesi.business;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.ejb.Stateful;
+import javax.persistence.EntityManager;
+import javax.persistence.LockModeType;
+import javax.persistence.PersistenceContext;
+import markesi.entity.User;
+
+/**
+ *
+ * @author LSV
+ */
+@Stateful
+public class UserEJB {
+    User user = null;
+    
+
+    @PersistenceContext(unitName = "MarkESI-PU")
+    private EntityManager em;
+
+    public User login(String username, String password) {
+        User usr = em.find(User.class, username);
+        if(usr.getPassword().equals(md5(password))){
+            this.user = usr;
+        }
+        return user;
+    }
+    
+    public void logout(){
+        this.user = null;
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    public User inscrire(String email, String username, String password, String nom, String prenom) {
+        User utilisateur = new User();
+        utilisateur.setNom(nom);
+        utilisateur.setPrenom(prenom);
+        utilisateur.setEmail(email);
+        //on genère le MD5 pour le password (histoire de pas le sauver en clair..)
+        utilisateur.setPassword(md5(password));
+        em.persist(utilisateur); 
+        this.user = utilisateur;
+        return utilisateur;
+    }
+    
+    public User getUser(){
+        return this.user;
+    }
+
+    public static String md5(String input) {
+        String md5 = null;
+        if (null == input) {
+            return null;
+        }
+        try {
+            //Create MessageDigest object for MD5
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            //Update input string in message digest
+            digest.update(input.getBytes(), 0, input.length());
+            //Converts message digest value in base 16 (hex)
+            md5 = new BigInteger(1, digest.digest()).toString(16);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return md5;
+    }
+}
