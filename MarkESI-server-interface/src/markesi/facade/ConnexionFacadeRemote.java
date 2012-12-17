b15cededd8ee25cdfe31cb7468df52e37ec18db9
unknown <G35309@L401P16.pdg.esi.heb.site>
2012-12-17 10:13:43 +0100

    merge branche AB

---
+++ MarkESI-server-interface/src/markesi/facade/ConnexionFacadeRemote.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package markesi.facade;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author G35309
+ */
+@Remote
+public interface ConnexionFacadeRemote {
+    
+}
---
+++ MarkESI-server/MarkESI-server-ejb/src/java/markesi/facade/ConnexionFacade.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package markesi.facade;
+
+import javax.ejb.Stateful;
+
+/**
+ *
+ * @author G35309
+ */
+@Stateful
+public class ConnexionFacade implements ConnexionFacadeRemote {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
